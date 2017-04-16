6cc55e0b552f537a7588661cb24777a87593a2dd
j0hnledez <Ledesmajohn24@yahoo.com>
2017-04-16 15:31:47 -0500

    Player Health and Health bar added

---
+++ Game2341_AIA/Assets/Scripts/Player/PlayerHeath.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine.UI; //access UI
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //load scenes
+
+public class PlayerHeath : MonoBehaviour {
+
+	public float startingHealth = 10f;
+	public Slider healthSlider; //access slider
+	public Image FillImage;
+	public Color fullHealthColor = Color.green;
+	public Color zeroHealthColor = Color.red;
+
+	PlayerController playerController; // access, can't move when health is 0
+	MouseLook mouseLook; //can't move camera when health hits zero
+
+	private float currentHealth;
+	private bool isDead;
+
+	void Awake()
+	{
+		playerController = GetComponent <PlayerController> ();
+		mouseLook = GetComponent <MouseLook> ();
+
+		currentHealth = startingHealth;
+		SetHealthUI (); //update UI
+
+		isDead = false;
+	}
+
+	public void TakeDamage(float amount) //set up health
+	{
+		currentHealth -= amount;
+
+		SetHealthUI ();
+
+		if (currentHealth <= 0f && !isDead) 
+		{
+			OnDeath ();
+		}
+	}
+
+	private void SetHealthUI()
+	{
+		healthSlider.value = currentHealth;
+
+		FillImage.color = 
+			Color.Lerp (zeroHealthColor, fullHealthColor, currentHealth / startingHealth);
+	}
+
+	private void OnDeath()
+	{
+		isDead = true;
+
+		playerController.enabled = false;
+		mouseLook.enabled = false;
+
+		SceneManager.LoadScene ("ISG_Level01_Lost");
+	}
+}
