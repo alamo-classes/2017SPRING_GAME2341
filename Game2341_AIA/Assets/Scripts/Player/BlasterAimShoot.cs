711cdd8df133962dfdeeb794b3d251082fea7141
Travis Womack <twomack4@student.alamo.edu>
2017-05-02 14:29:34 -0500

    Tank hits have effects and sound

--- Game2341_AIA/Assets/Scripts/Player/BlasterAimShoot.cs
+++ Game2341_AIA/Assets/Scripts/Player/BlasterAimShoot.cs
@@ -4,67 +4,69 @@ using UnityEngine.SceneManagement;
 
 public class BlasterAimShoot : MonoBehaviour
 {
-   public int gunDamage = 5;
-   public float fireRate = 0.1f;
-   public float weaponRange = 200f;
-   public float hitForce = 100f;
-   public Transform gunEnd;
-   public Camera aimCam;
+	public int gunDamage = 5;
+	public float fireRate = 0.1f;
+	public float weaponRange = 200f;
+	public float hitForce = 100f;
+	public Transform gunEnd;
+	public Camera aimCam;
 
-   private WaitForSeconds shotDuration = new WaitForSeconds (0.07f);
-   private AudioSource gunAudio;
-   private LineRenderer laserLine;
-   private float nextFire;
+	private WaitForSeconds shotDuration = new WaitForSeconds (0.07f);
+	private AudioSource gunAudio;
+	private LineRenderer laserLine;
+	private float nextFire;
 
-   void Start ()
-   {
-      laserLine = GetComponent<LineRenderer> ();
-      gunAudio = GetComponent<AudioSource> ();
-   }
+	void Start ()
+	{
+		laserLine = GetComponent<LineRenderer> ();
+		gunAudio = GetComponent<AudioSource> ();
+	}
 
 
-   void Update ()
-   {
-      if (Input.GetButtonDown ("Fire1") && (Time.time > nextFire))
-      {
-         nextFire = Time.time + fireRate;
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Fire1") && (Time.time > nextFire))
+		{
+			nextFire = Time.time + fireRate;
 
-         StartCoroutine (ShotEffect ());
+			StartCoroutine (ShotEffect ());
 
-         Vector3 rayOrigin = aimCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
-         RaycastHit hit;
-         laserLine.SetPosition (0, gunEnd.position);
+			Vector3 rayOrigin = aimCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+			RaycastHit hit;
+			laserLine.SetPosition (0, gunEnd.position);
 
-         if (Physics.Raycast (rayOrigin, aimCam.transform.forward, out hit, weaponRange))
-         {
-            laserLine.SetPosition (1, hit.point);
-            TankHealth health = hit.collider.GetComponent<TankHealth> ();
+			if (Physics.Raycast (rayOrigin, aimCam.transform.forward, out hit, weaponRange))
+			{
+				laserLine.SetPosition (1, hit.point);
+				TankHealth health = hit.collider.GetComponent<TankHealth> ();
+				//Debug.Log ("Shot fired at tank |" + hit.collider + "| " + gunDamage + " |" + health +"|");
 
-            if (health != null)
-            {
-               health.TakeDamage (gunDamage);
-               UIManager.score += 50;
-            }
+				if (health != null)
+				{
+					health.TakeDamage (gunDamage);
+					UIManager.score += 50;
+				}
 
-         } else
-         {
-            laserLine.SetPosition (1, rayOrigin + (aimCam.transform.forward * weaponRange));
-         }
-      }
-   }
+			} else
+			{
+				laserLine.SetPosition (1, rayOrigin + (aimCam.transform.forward * weaponRange));
+			}
+		}
+	}
 
-   private IEnumerator ShotEffect ()
-   {
-      // Play the shooting sound effect
-      gunAudio.Play ();
+	private IEnumerator ShotEffect ()
+	{
+		// Play the shooting sound effect
+		gunAudio.Play ();
 
-      // Turn on our line renderer
-      laserLine.enabled = true;
+		// Turn on our line renderer
+		laserLine.enabled = true;
 
-      //Wait for .07 seconds
-      yield return shotDuration;
+		//Wait for .07 seconds
+		yield return shotDuration;
 
-      // Deactivate our line renderer after waiting
-      laserLine.enabled = false;
-   }
+		// Deactivate shot effect
+		laserLine.enabled = false;
+		gunAudio.Stop ();
+	}
 }
--- Game2341_AIA/Assets/Scripts/Tank/TankHealth.cs
+++ Game2341_AIA/Assets/Scripts/Tank/TankHealth.cs
@@ -4,94 +4,95 @@ using System.Collections;
 
 public class TankHealth : MonoBehaviour
 {
-   public float startingHealth = 12f;
-   // The amount of health each tank starts with.
-   public GameObject explosionPrefab;
-   // A prefab that will be instantiated in Awake, then used whenever the tank dies.
-   public GameObject tankHit;
+	public float startingHealth = 12f;
+	// The amount of health each tank starts with.
+	public GameObject explosionPrefab;
+	// A prefab that will be instantiated in Awake, then used whenever the tank dies.
+	public GameObject tankHit;
 
-   private AudioSource explosionAudio;
-   // The audio source to play when the tank explodes.
-   private AudioSource hitAudio;
-   private ParticleSystem explosionParticles;
-   // The particle system the will play when the tank is destroyed.
-   private ParticleSystem hitParticles;
-   //private TankMove tankControl;
+	private AudioSource explosionAudio;
+	// The audio source to play when the tank explodes.
+	private AudioSource hitAudio;
+	private ParticleSystem explosionParticles;
+	// The particle system the will play when the tank is destroyed.
+	private ParticleSystem hitParticles;
+	//private TankMove tankControl;
 
-   private float currentHealth;
-   // How much health the tank currently has.
-   private bool dead;
-   // Has the tank been reduced beyond zero health yet?
+	private float currentHealth;
+	// How much health the tank currently has.
+	private bool dead;
+	// Has the tank been reduced beyond zero health yet?
 
-   private void Awake ()
-   {
-      // Instantiate the explosion prefab and get a reference to the particle system on it.
-      explosionParticles = Instantiate (explosionPrefab).GetComponent<ParticleSystem> ();
-      hitParticles = Instantiate (tankHit).GetComponent<ParticleSystem> ();
+	private void Awake ()
+	{
+		// Instantiate the explosion prefab and get a reference to the particle system on it.
+		explosionParticles = Instantiate (explosionPrefab).GetComponent<ParticleSystem> ();
+		hitParticles = Instantiate (tankHit).GetComponent<ParticleSystem> ();
 
-      // Get a reference to the audio source on the instantiated prefab.
-      explosionAudio = explosionParticles.GetComponent<AudioSource> ();
-      hitAudio = hitParticles.GetComponent<AudioSource> ();
+		// Get a reference to the audio source on the instantiated prefab.
+		explosionAudio = explosionParticles.GetComponent<AudioSource> ();
+		hitAudio = hitParticles.GetComponent<AudioSource> ();
 
-      // Disable the prefab so it can be activated when it's required.
-      explosionParticles.gameObject.SetActive (false);
-      hitParticles.gameObject.SetActive (false);
+		// Disable the prefab so it can be activated when it's required.
+		explosionParticles.gameObject.SetActive (false);
+		hitParticles.gameObject.SetActive (false);
 
-      //tankControl = GetComponent<TankMove> ();
-   }
-
-
-   private void OnEnable ()
-   {
-      // When the tank is enabled, reset the tank's health and whether or not it's dead.
-      currentHealth = startingHealth;
-      dead = false;
-   }
+		//tankControl = GetComponent<TankMove> ();
+	}
 
 
-   public void TakeDamage (float amount)
-   {
-      // Reduce current health by the amount of damage done.
-      currentHealth -= amount;
+	private void OnEnable ()
+	{
+		// When the tank is enabled, reset the tank's health and whether or not it's dead.
+		currentHealth = startingHealth;
+		dead = false;
+	}
 
-      // If the current health is at or below zero and it has not yet been registered, call OnDeath.
-      if (currentHealth <= 0f && !dead)
-      {
-         OnDeath ();
-      } else
-      {
-         hitParticles.gameObject.SetActive (false); 
-         // Move the instantiated explosion prefab to the tank's position and turn it on.
-         hitParticles.transform.position = transform.position;
-         hitParticles.gameObject.SetActive (true);
 
-         // Play the particle system of the tank exploding.
-         hitParticles.Play ();
+	public void TakeDamage (float amount)
+	{
+		// Reduce current health by the amount of damage done.
+		currentHealth -= amount;
+		//Debug.Log ("Tank hit");
 
-         // Play the tank explosion sound effect.
-         hitAudio.Play ();
-      }
-   }
+		// If the current health is at or below zero and it has not yet been registered, call OnDeath.
+		if (currentHealth <= 0f && !dead)
+		{
+			OnDeath ();
+		} else
+		{
+			hitParticles.gameObject.SetActive (false); 
+			// Move the instantiated explosion prefab to the tank's position and turn it on.
+			hitParticles.transform.position = transform.position;
+			hitParticles.gameObject.SetActive (true);
 
-   private void OnDeath ()
-   {
-      // Set the flag to call function once.
-      dead = true;
-      // turn off to make sure it plays correctly
-      explosionParticles.gameObject.SetActive (false); 
-      // Move the instantiated explosion prefab to the tank's position and turn it on.
-      explosionParticles.transform.position = transform.position;
-      explosionParticles.gameObject.SetActive (true);
+			// Play the particle system of the tank exploding.
+			hitParticles.Play ();
 
-      // Play the particle system of the tank exploding.
-      explosionParticles.Play ();
+			// Play the tank explosion sound effect.
+			hitAudio.Play ();
+		}
+	}
 
-      // Play the tank explosion sound effect.
-      explosionAudio.Play ();
+	private void OnDeath ()
+	{
+		// Set the flag to call function once.
+		dead = true;
+		// turn off to make sure it plays correctly
+		explosionParticles.gameObject.SetActive (false); 
+		// Move the instantiated explosion prefab to the tank's position and turn it on.
+		explosionParticles.transform.position = transform.position;
+		explosionParticles.gameObject.SetActive (true);
 
-      //yield return playTime;
+		// Play the particle system of the tank exploding.
+		explosionParticles.Play ();
 
-      // Turn the tank off.
-      gameObject.SetActive (false);
-   }
+		// Play the tank explosion sound effect.
+		explosionAudio.Play ();
+
+		//yield return playTime;
+
+		// Turn the tank off.
+		gameObject.SetActive (false);
+	}
 }
