4446238f00e5c435fc02b3cf9ac82561f53daebf
Travis Womack <twomack4@student.alamo.edu>
2017-05-05 17:19:25 -0500

    Revert "Spawn Points"
    This reverts commit 84bc01bfd5adb3393cd2c05723d04ef5494b047b.

---
+++ Game2341_AIA/Assets/Scripts/Managers/YouLoose.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class YouLoose : MonoBehaviour {
+    public int loseScene;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(loseScene);
+        }
+    }
+}
