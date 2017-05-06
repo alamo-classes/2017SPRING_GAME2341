e16191847a701f29da5139452f60b7869025b07c
Travis Womack <twomack4@student.alamo.edu>
2017-05-06 09:56:49 -0500

    Beta version - sound and spawn points

---
+++ Game2341_AIA/Assets/Scripts/Tank/EnemyMag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyMag : MonoBehaviour {  
+	public GameObject Tank; // the object that will be  using the  points            
+	public float spawnTime = 3f;// the  amount  of  time the tank will spawn made  it public for  easy editing           
+	public Transform[] spawnPoints; //  the  points  were the  spawn from        
+
+	void Start ()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		//  calls for  the  spawn function at the start of  the  game 
+	}
+		
+	void Spawn ()
+	{
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		//  founds the points between the spawn points 
+		//Debug.Log("Tank are spawn");
+
+		Instantiate (Tank, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			// create a  instantiate for  the tank for  the  spawn point position and rotation. 
+	}
+}
